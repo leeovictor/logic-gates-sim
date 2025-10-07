eaea74de0c9aa1c2791e9e971ea8defdc3ac1495
Leonardo Victor <leonardovictormarques@gmail.com>
2025-10-07 20:03:19 -0300

    config canvas

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
-console.log('Hello, Logic Gates Simulator!');
+const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d')!
+
+ctx.fillStyle = 'rgb(200, 3, 230)';
+ctx.beginPath();
+ctx.moveTo(100, 100);
+ctx.lineTo(200, 100)
+ctx.lineTo(150, 200)
+ctx.fill();
