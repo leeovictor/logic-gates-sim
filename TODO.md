fa63700615e6d4f9d25996cdde51956cf2877246
Leonardo Victor <leonardovictormarques@gmail.com>
2025-10-07 20:58:58 -0300

    add toolbar inicial e ajustes em formatação

--- TODO.md
+++ TODO.md
@@ -1,7 +1,7 @@
-* configurar canvas
+-   configurar canvas
 
-* definir como será feita a simulação das portas logicas (tick? instantanea? propagação de sinal?)
+-   definir como será feita a simulação das portas logicas (tick? instantanea? propagação de sinal?)
 
-* adicionar e remover portas logicas
-* conectar portas lógicas 
-* visualizar resultados do circuito
+-   adicionar e remover portas logicas
+-   conectar portas lógicas
+-   visualizar resultados do circuito
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
+import './style.css';
 import orImg from './images/or.png';
 import andImg from './images/and.png';
 
 const canvas = document.getElementById('canvas') as HTMLCanvasElement;
-const ctx = canvas.getContext('2d')!
+const ctx = canvas.getContext('2d')!;
 
 function loadImage(imgPath: string): Promise<HTMLImageElement> {
     return new Promise((resolve, reject) => {
@@ -15,10 +16,9 @@ function loadImage(imgPath: string): Promise<HTMLImageElement> {
 
 async function main() {
     const or = await loadImage(orImg);
-    const and = await loadImage(andImg)
-    
-    ctx.rotate(Math.PI / 180 * 15);
-    ctx.translate(250, 0);
+    const and = await loadImage(andImg);
+
+    ctx.translate(250, 200);
     ctx.drawImage(or, 0, 0, 120, 120);
     ctx.drawImage(and, 150, 0, 120, 120);
 }
